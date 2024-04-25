e129a803b48aad713ac25393ad4cdf602ad6bed3
Marcel <mibes@avaya.com>
2024-04-25 21:31:18 +0200

    Explicit --crypto=openssl

--- bindings.rs
+++ bindings.rs
@@ -1,7 +1,7 @@
 //!
 //! XmlSec Bindings Generation
 //!
-use bindgen::Builder   as BindgenBuilder;
+use bindgen::Builder as BindgenBuilder;
 use bindgen::Formatter as BindgenFormatter;
 
 use pkg_config::Config as PkgConfig;
@@ -10,23 +10,19 @@ use std::env;
 use std::path::PathBuf;
 use std::process::Command;
 
-
 const BINDINGS: &str = "bindings.rs";
 
+fn main() {
+    println!("cargo:rustc-link-lib=xmlsec1-openssl"); // -lxmlsec1-openssl
+    println!("cargo:rustc-link-lib=xmlsec1"); // -lxmlsec1
+    println!("cargo:rustc-link-lib=xml2"); // -lxml2
+    println!("cargo:rustc-link-lib=ssl"); // -lssl
+    println!("cargo:rustc-link-lib=crypto"); // -lcrypto
 
-fn main()
-{
-    println!("cargo:rustc-link-lib=xmlsec1-openssl");  // -lxmlsec1-openssl
-    println!("cargo:rustc-link-lib=xmlsec1");          // -lxmlsec1
-    println!("cargo:rustc-link-lib=xml2");             // -lxml2
-    println!("cargo:rustc-link-lib=ssl");              // -lssl
-    println!("cargo:rustc-link-lib=crypto");           // -lcrypto
-
-    let path_out      = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let path_out = PathBuf::from(env::var("OUT_DIR").unwrap());
     let path_bindings = path_out.join(BINDINGS);
 
-    if !path_bindings.exists()
-    {
+    if !path_bindings.exists() {
         PkgConfig::new()
             .probe("xmlsec1")
             .expect("Could not find xmlsec1 using pkg-config");
@@ -39,18 +35,17 @@ fn main()
             .formatter(BindgenFormatter::default())
             .generate_comments(true);
 
-        let bindings = bindbuild.generate()
-            .expect("Unable to generate bindings");
+        let bindings = bindbuild.generate().expect("Unable to generate bindings");
 
-        bindings.write_to_file(path_bindings)
+        bindings
+            .write_to_file(path_bindings)
             .expect("Couldn't write bindings!");
     }
 }
 
-
-fn fetch_xmlsec_config_flags() -> Vec<String>
-{
+fn fetch_xmlsec_config_flags() -> Vec<String> {
     let out = Command::new("xmlsec1-config")
+        .arg("--crypto=openssl")
         .arg("--cflags")
         .output()
         .expect("Failed to get --cflags from xmlsec1-config. Is xmlsec1 installed?")
@@ -59,10 +54,9 @@ fn fetch_xmlsec_config_flags() -> Vec<String>
     args_from_output(out)
 }
 
-
-fn fetch_xmlsec_config_libs() -> Vec<String>
-{
+fn fetch_xmlsec_config_libs() -> Vec<String> {
     let out = Command::new("xmlsec1-config")
+        .arg("--crypto=openssl")
         .arg("--libs")
         .output()
         .expect("Failed to get --libs from xmlsec1-config. Is xmlsec1 installed?")
@@ -71,13 +65,11 @@ fn fetch_xmlsec_config_libs() -> Vec<String>
     args_from_output(out)
 }
 
+fn args_from_output(args: Vec<u8>) -> Vec<String> {
+    let decoded = String::from_utf8(args).expect("Got invalid UTF8 from xmlsec1-config");
 
-fn args_from_output(args: Vec<u8>) -> Vec<String>
-{
-    let decoded = String::from_utf8(args)
-        .expect("Got invalid UTF8 from xmlsec1-config");
-
-    let args = decoded.split_whitespace()
+    let args = decoded
+        .split_whitespace()
         .map(|p| p.to_owned())
         .collect::<Vec<String>>();
 
