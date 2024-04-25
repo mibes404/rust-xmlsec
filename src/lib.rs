217d967cb7e76a7c97c0d911859aa8115e2ce68c
Marcel <mibes@avaya.com>
2024-04-25 10:22:37 +0200

    Fix tests and example

--- src/lib.rs
+++ src/lib.rs
@@ -4,36 +4,36 @@
 //! Modules reflect the header names of the bound xmlsec1 library
 //!
 #![deny(missing_docs)]
-
 #![deny(clippy::all)]
-#![allow(clippy::new_without_default)]  // simply do not agree with this
+#![allow(clippy::new_without_default)] // simply do not agree with this
 
 // imports
 use lazy_static::lazy_static;
 
 #[doc(hidden)]
-pub use libxml::tree::node::Node as XmlNode;
-#[doc(hidden)]
 pub use libxml::tree::document::Document as XmlDocument;
+#[doc(hidden)]
+pub use libxml::tree::node::Node as XmlNode;
 #[doc(hidden)]
 pub use libxml::xpath::Context as XmlXPathContext;
 
 // internals
+mod crypto;
+mod error;
 mod exts;
 mod keys;
-mod error;
-mod crypto;
-mod xmlsec;
-mod xmldsig;
 mod templates;
 mod transforms;
+mod xmldsig;
+mod xmlsec;
 
 // exports
-pub mod bindings;  // for external extending of this wrapper
+pub mod bindings; // for external extending of this wrapper
 
 pub use self::exts::XmlSecDocumentExt;
 
 pub use self::keys::XmlSecKey;
+pub use self::keys::XmlSecKeyDataType;
 pub use self::keys::XmlSecKeyFormat;
 
 pub use self::error::XmlSecError;
@@ -43,9 +43,9 @@ pub use self::crypto::XmlSecSignatureMethod;
 
 pub use self::xmldsig::XmlSecSignatureContext;
 
-pub use self::templates::TemplateBuilder            as XmlSecTemplateBuilder;
-pub use self::templates::XmlDocumentTemplating      as XmlSecDocumentTemplating;
+pub use self::templates::TemplateBuilder as XmlSecTemplateBuilder;
 pub use self::templates::XmlDocumentTemplateBuilder as XmlSecDocumentTemplateBuilder;
+pub use self::templates::XmlDocumentTemplating as XmlSecDocumentTemplating;
 
 pub use self::transforms::XmlSecCanonicalizationMethod;
 
@@ -55,9 +55,9 @@ pub mod template {
 
     pub mod preamble {
         //! Preamble of all things signature template creation.
-        pub use crate::XmlSecTemplateBuilder;
-        pub use crate::XmlSecDocumentTemplating;
         pub use crate::XmlSecCanonicalizationMethod;
+        pub use crate::XmlSecDocumentTemplating;
         pub use crate::XmlSecSignatureMethod;
+        pub use crate::XmlSecTemplateBuilder;
     }
 }
--- tests/test_keys.rs
+++ tests/test_keys.rs
@@ -2,23 +2,25 @@
 //! Unit Tests for Key
 //!
 use xmlsec::XmlSecKey;
+use xmlsec::XmlSecKeyDataType;
 use xmlsec::XmlSecKeyFormat;
 
-
 #[test]
-fn test_key_cert_loading_file()
-{
-    let key = XmlSecKey::from_file("tests/resources/key.pem", XmlSecKeyFormat::Pem, None)
-        .expect("Failed to properly load key for test");
+fn test_key_cert_loading_file() {
+    let key = XmlSecKey::from_file(
+        "tests/resources/key.pem",
+        XmlSecKeyFormat::Pem,
+        XmlSecKeyDataType::Private,
+        None,
+    )
+    .expect("Failed to properly load key for test");
 
     key.load_cert_from_file("tests/resources/key.crt", XmlSecKeyFormat::Pem)
         .expect("Failed to properly load key certificate for test");
 }
 
-
 #[test]
-fn test_key_cert_loading_memory()
-{
+fn test_key_cert_loading_memory() {
     let keybuff = std::fs::read("tests/resources/key.pem")
         .expect("Failed to read file for testing key/cert from memory load");
 
@@ -32,12 +34,15 @@ fn test_key_cert_loading_memory()
         .expect("Failed to properly load key certificate for test");
 }
 
-
 #[test]
-fn test_key_name_handing()
-{
-    let mut key = XmlSecKey::from_file("tests/resources/key.pem", XmlSecKeyFormat::Pem, None)
-        .expect("Failed to properly load key for test");
+fn test_key_name_handing() {
+    let mut key = XmlSecKey::from_file(
+        "tests/resources/key.pem",
+        XmlSecKeyFormat::Pem,
+        XmlSecKeyDataType::Private,
+        None,
+    )
+    .expect("Failed to properly load key for test");
 
     key.set_name("testname");
 
